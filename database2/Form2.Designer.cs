0ecf42c1ea24e5ab5b4dceca1650ed702a333ddf
Anicka <Anicka2008@gmail.com>
2021-09-19 20:40:29 +0430

    the 4th committing

--- database2/Form2.Designer.cs
+++ database2/Form2.Designer.cs
@@ -30,10 +30,11 @@ namespace database2
         private void InitializeComponent()
         {
             this.tabControl = new System.Windows.Forms.TabControl();
-            this.tabPage1 = new System.Windows.Forms.TabPage();
             this.tabPage2 = new System.Windows.Forms.TabPage();
-            this.btnSelect = new System.Windows.Forms.Button();
+            this.flowLayoutPanel = new System.Windows.Forms.FlowLayoutPanel();
+            this.btnsave = new System.Windows.Forms.Button();
             this.tabControl.SuspendLayout();
+            this.tabPage2.SuspendLayout();
             this.SuspendLayout();
             // 
             // tabControl
@@ -41,7 +42,6 @@ namespace database2
             this.tabControl.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.tabControl.Controls.Add(this.tabPage1);
             this.tabControl.Controls.Add(this.tabPage2);
             this.tabControl.Font = new System.Drawing.Font("Comic Sans MS", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.tabControl.Location = new System.Drawing.Point(14, 66);
@@ -50,50 +50,48 @@ namespace database2
             this.tabControl.Size = new System.Drawing.Size(564, 498);
             this.tabControl.TabIndex = 1;
             // 
-            // tabPage1
-            // 
-            this.tabPage1.AutoScroll = true;
-            this.tabPage1.BackColor = System.Drawing.Color.LightPink;
-            this.tabPage1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
-            this.tabPage1.Font = new System.Drawing.Font("Comic Sans MS", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.tabPage1.Location = new System.Drawing.Point(4, 33);
-            this.tabPage1.Name = "tabPage1";
-            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage1.Size = new System.Drawing.Size(556, 461);
-            this.tabPage1.TabIndex = 0;
-            this.tabPage1.Text = "Products";
-            // 
             // tabPage2
             // 
+            this.tabPage2.BackColor = System.Drawing.Color.LightPink;
+            this.tabPage2.Controls.Add(this.flowLayoutPanel);
             this.tabPage2.Location = new System.Drawing.Point(4, 33);
             this.tabPage2.Name = "tabPage2";
             this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
             this.tabPage2.Size = new System.Drawing.Size(556, 461);
             this.tabPage2.TabIndex = 1;
-            this.tabPage2.Text = "tabPage2";
-            this.tabPage2.UseVisualStyleBackColor = true;
+            this.tabPage2.Text = "Products";
             // 
-            // btnSelect
+            // flowLayoutPanel
             // 
-            this.btnSelect.Location = new System.Drawing.Point(209, 17);
-            this.btnSelect.Name = "btnSelect";
-            this.btnSelect.Size = new System.Drawing.Size(75, 43);
-            this.btnSelect.TabIndex = 2;
-            this.btnSelect.Text = "select";
-            this.btnSelect.UseVisualStyleBackColor = true;
-            this.btnSelect.Click += new System.EventHandler(this.btnSelect_Click);
+            this.flowLayoutPanel.Location = new System.Drawing.Point(6, 6);
+            this.flowLayoutPanel.Name = "flowLayoutPanel";
+            this.flowLayoutPanel.Size = new System.Drawing.Size(515, 424);
+            this.flowLayoutPanel.TabIndex = 0;
+            // 
+            // btnsave
+            // 
+            this.btnsave.BackColor = System.Drawing.Color.White;
+            this.btnsave.Font = new System.Drawing.Font("Comic Sans MS", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnsave.Location = new System.Drawing.Point(108, 66);
+            this.btnsave.Name = "btnsave";
+            this.btnsave.Size = new System.Drawing.Size(79, 33);
+            this.btnsave.TabIndex = 2;
+            this.btnsave.Text = "Save";
+            this.btnsave.UseVisualStyleBackColor = false;
+            this.btnsave.Click += new System.EventHandler(this.btnsave_Click);
             // 
             // Form2
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(625, 576);
-            this.Controls.Add(this.btnSelect);
+            this.Controls.Add(this.btnsave);
             this.Controls.Add(this.tabControl);
             this.Name = "Form2";
             this.Text = "Form2";
             this.Load += new System.EventHandler(this.Form2_Load);
             this.tabControl.ResumeLayout(false);
+            this.tabPage2.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -101,8 +99,8 @@ namespace database2
         #endregion
 
         private System.Windows.Forms.TabControl tabControl;
-        private System.Windows.Forms.TabPage tabPage1;
         private System.Windows.Forms.TabPage tabPage2;
-        private System.Windows.Forms.Button btnSelect;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel;
+        private System.Windows.Forms.Button btnsave;
     }
 }
--- database2/Form2.cs
+++ database2/Form2.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Data.SqlClient;
+using System.Diagnostics;
 using System.Drawing;
 using System.IO;
 using System.Linq;
@@ -15,6 +16,7 @@ namespace database2
 {
     public partial class Form2 : Form
     {
+
 
         public Form2()
         {
@@ -29,198 +31,145 @@ namespace database2
 
         private void Form2_Load(object sender, EventArgs e)
         {
-            tabPage1.Controls.Clear();
+            flowLayoutPanel.Controls.Clear();
+            flowLayoutPanel.AutoScroll = true;
+            flowLayoutPanel.Dock = DockStyle.Fill;
 
-            string noNullProperty(int i)
-            {
-                if (!string.IsNullOrEmpty(output[i].Printer))
-                { return output[i].Printer; }
-                if (!string.IsNullOrEmpty(output[i].Laser))
-                { return output[i].Laser; }
-                else { return output[i].Laminator; }
-            }
 
-            int x = 0, y = 0;
-            int j = 0;
+
+            //    if ((output[i].Pic).GetType() == (typeof(byte[])))
+            //    {
+            //        imgbyte = pI.Pic;
+            //    }
+            //    // else if ((output[i].Pic).GetType() == (typeof(string)))
+            //    if (output[i].Id == 6)
+            //    {
+            //        MessageBox.Show("y");
+            //        // imgbyte = Convert.FromBase64String(output[i].Pic);
+            //    }
+
+            //    using (MemoryStream ms = new MemoryStream(imgbyte))
+            //    {
+            //        Image img = Image.FromStream(ms);
+            //        pic.Image = img;
+            //    }
+
+         
+
+            //var result = from s in gbList
+            //             where s.Focused == true
+            //             //where s.selct()
+            //             select s;
+
+            //    foreach (var s in result)
+            //{}
+            
+        }
+        string noNullProperty(int i)
+        {
+            if (!string.IsNullOrEmpty(output[i].Printer))
+            { return output[i].Printer; }
+            if (!string.IsNullOrEmpty(output[i].Laser))
+            { return output[i].Laser; }
+            else { return output[i].Laminator; }
+        }
+        private const int imageWidth = 128;
+        private const int imageHeight = 128;
+
+        protected override void OnLoad(EventArgs e)
+        {
+            base.OnLoad(e);
             for (int i = 0; i < output.Count - 1; i++)
             {
-                SelectablePictureBox pic = new SelectablePictureBox();
-                pic.SizeMode = PictureBoxSizeMode.StretchImage;
-               // GroupBox gb = new GroupBox();
-                SelectablegroupBox gb = new SelectablegroupBox();
-                gb.Controls.Add(pic);
-                //pic.Location= new Point()
-                pic.Dock = DockStyle.Right;
-                gb.Text = noNullProperty(i);
-
-                gbList.Add(gb);
-
+                CheckBox cb = new CheckBox();
+                cb.Appearance = Appearance.Button;
+                cb.Size = new Size(imageWidth, imageHeight);
+                cb.BackgroundImageLayout = ImageLayout.Zoom;
                 ProductImages pI = output[i];
 
-                if ((output[i].Pic).GetType() == (typeof(byte[])))
-                {
-                    imgbyte = pI.Pic;
-                }
-                // else if ((output[i].Pic).GetType() == (typeof(string)))
-                if (output[i].Id == 6)
-                {
-                    MessageBox.Show("y");
-                    // imgbyte = Convert.FromBase64String(output[i].Pic);
-                }
+                imgbyte = pI.Pic;
 
-                using (MemoryStream ms = new MemoryStream(imgbyte))
-                {
-                    Image img = Image.FromStream(ms);
-                    pic.Image = img;
-                }
-
-                tabPage1.Controls.Add(gbList[j]);
-                gbList[j].Location = new Point(x, y);
-                //x += gbList[i].Width;
-                y += gbList[i].Height;
-                j++;
-                //UserSelecedItems userItems = new UserSelecedItems();
-            }
-
-
-            // private void btnSelect_Click_1(object sender, EventArgs e)
-            // {
-            // for ( j = 0; j < gbList.Count; j++)
-            //{
-           // MessageBox.Show(gbList.Count.ToString());
-            
-            var result = from s in gbList
-                         where s.Focused == true
-                         //where s.selct()
-                         select s;
-
-                foreach (var s in result)
-            {
-                MessageBox.Show(gbList.Count.ToString());
-                string sql = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\114\source\repos\database2\database2\Mock_Data.mdf;Integrated Security=True";
-
-
-                    using (IDbConnection con = new SqlConnection(sql))
-                    {
-                        List<UserSelecedItems> userItems = new List<UserSelecedItems>();
-                        userItems.Add(new UserSelecedItems { Id = 1, Item = s.Text }); ;
-
-                        con.Execute("insert into UserItem(Id, Item) values(@Id,  @Item)", userItems);
-                    }
-                }
-                //    Console.WriteLine(student.Id + ", " + student.Name);
-                //if (gbList[j].Focused)
+                //using (MemoryStream ms = new MemoryStream(imgbyte))
                 //{
-                //     MessageBox.Show("f");
-                //   // MessageBox.Show(gbList.Count.ToString());
-                //    string sql = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\114\source\repos\database2\database2\Mock_Data.mdf;Integrated Security=True";
+                //    Image img = Image.FromStream(ms);
+                //    cb.BackgroundImage = img;
+                //    //}
+                //Don't dispose the MemoryStream, the Image class will need it!
+                var ms = new MemoryStream(pI.Pic);
+                cb.BackgroundImage = Image.FromStream(ms);
+                cb.Text = noNullProperty(i);
+                flowLayoutPanel.Controls.Add(cb);
 
 
-                //    using (IDbConnection con = new SqlConnection(sql))
-                //    {//, im = pic.Image
-                //        List<UserSelecedItems> userItems = new List<UserSelecedItems>();
-                //         userItems.Add(new UserSelecedItems { Id = 1, Item = gbList[j].Text }); ;
-
-                //         con.Execute("insert into UserItem(Id, Item) values(@Id,  @Item)", userItems);
-                //    }
-                //}
-           // }
+            }
         }
 
-        private void btnSelect_Click(object sender, EventArgs e)
+
+        private void btnsave_Click(object sender, EventArgs e)
         {
-            for (int j = 0; j < gbList.Count; j++)
+            int j = 1;
+            
+            var selected = flowLayoutPanel.Controls.OfType<CheckBox>().Where(x => x.Checked);
+            if (selected != null)
             {
-              
-                if (gbList[j].Focused)
+                Debug.Print("Selected images: {0}", selected.Count());
+                foreach (var item in selected)
                 {
-                    MessageBox.Show("f");
-                    // MessageBox.Show(gbList.Count.ToString());
+                    //Save the picture from item.BackgroundImage.
                     string sql = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\114\source\repos\database2\database2\Mock_Data.mdf;Integrated Security=True";
 
 
                     using (IDbConnection con = new SqlConnection(sql))
                     {
                         List<UserSelecedItems> userItems = new List<UserSelecedItems>();
-                        userItems.Add(new UserSelecedItems { Id = 1, Item = gbList[j].Text }); ;
-
+                        userItems.Add(new UserSelecedItems { Id = j, Item = item.Text });
+                        j++;
                         con.Execute("insert into UserItem(Id, Item) values(@Id,  @Item)", userItems);
                     }
                 }
+                MessageBox.Show("Done!");
             }
+            else { MessageBox.Show("Nothing to save!"); }
         }
     }
-    class UserSelecedItems
-    {
-        //public Image im { get; set; }
-        //public string Name { get; set; }
-        public string Item { get; set; }
-        public int Id { get; set; }
+        class UserSelecedItems
+        {
+            //public Image im { get; set; }
+            //public string Name { get; set; }
+            public string Item { get; set; }
+            public int Id { get; set; }
+        }
+
+     
+        //class SelectablegroupBox : GroupBox
+        //{
+        //    public SelectablegroupBox()
+        //    {
+        //        this.SetStyle(ControlStyles.Selectable, true);
+        //        this.TabStop = true;
+        //    }
+
+        //    protected override void OnEnter(EventArgs e)
+        //    {
+        //        //
+        //        this.Focus();
+        //        this.Invalidate();
+        //        base.OnEnter(e);
+        //    }
+
+        //    protected override void OnPaint(PaintEventArgs pe)
+        //    {
+        //        base.OnPaint(pe);
+        //        if (this.Focused)
+        //        {
+        //            var rc = this.ClientRectangle;
+        //            rc.Inflate(-2, -2);
+        //            ControlPaint.DrawFocusRectangle(pe.Graphics, rc);
+        //        }
+        //    }
+        //}
+
     }
 
-    class SelectablePictureBox : PictureBox
-    {
-        public SelectablePictureBox()
-        {
-            this.SetStyle(ControlStyles.Selectable, true);
-            this.TabStop = true;
-        }
-        protected override void OnMouseDown(MouseEventArgs e)
-        {
-            this.Focus();
-            base.OnMouseDown(e);
-        }
-        protected override void OnEnter(EventArgs e)
-        {
-            //
-            this.Focus();
-            this.Invalidate();
-            base.OnEnter(e);
-        }
-        protected override void OnLeave(EventArgs e)
-        {
-            this.Invalidate();
-            base.OnLeave(e);
-        }
-        protected override void OnPaint(PaintEventArgs pe)
-        {
-            base.OnPaint(pe);
-            if (this.Focused)
-            {
-                var rc = this.ClientRectangle;
-                rc.Inflate(-2, -2);
-                ControlPaint.DrawFocusRectangle(pe.Graphics, rc);
-            }
-        }
-    }
 
-    class SelectablegroupBox : GroupBox
-    {
-        public SelectablegroupBox()
-        {
-            this.SetStyle(ControlStyles.Selectable, true);
-            this.TabStop = true;
-        }
-       
-        protected override void OnEnter(EventArgs e)
-        {
-            //
-            this.Focus();
-            this.Invalidate();
-            base.OnEnter(e);
-        }
-       
-        protected override void OnPaint(PaintEventArgs pe)
-        {
-            base.OnPaint(pe);
-            if (this.Focused)
-            {
-                var rc = this.ClientRectangle;
-                rc.Inflate(-2, -2);
-                ControlPaint.DrawFocusRectangle(pe.Graphics, rc);
-            }
-        }
-    }
-}
 
-    
